40f1c42bb7514d93415fbe57611613dac50b332c
Fatema Saifee <fatema.saifee711@gmail.com>
2018-12-12 09:08:38 -0500

    Create helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+# Python 3.x code to demonstrate Hello World!
+
+# Function to demonstrate printing a statement 
+def printStatement(statement): 
+    print(statement) 
+
+# Driver Code 
+statement = "Hello World!"
+printStatement(statement)
